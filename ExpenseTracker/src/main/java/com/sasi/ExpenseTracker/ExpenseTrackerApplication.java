26ed27fa3c6541f54d9267a2e0a9bac9d8a02abc
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-01-23 19:02:04 +0530

    swagger added

--- ExpenseTracker/src/main/java/com/sasi/ExpenseTracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/com/sasi/ExpenseTracker/ExpenseTrackerApplication.java
@@ -3,11 +3,14 @@ package com.sasi.ExpenseTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
-	
+
 }
